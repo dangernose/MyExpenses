05802d56d50d4f5cb85951264e5626416b0d3325
dangernose <dangernose@gmail.com>
2018-08-28 17:29:27 +0545

    New Activity and Class Added

---
+++ app/src/main/java/com/suresh/expensesapp/ExpensesListActivity.java
@@ -0,0 +1,13 @@
+package com.suresh.expensesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpensesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses_list);
+    }
+}
